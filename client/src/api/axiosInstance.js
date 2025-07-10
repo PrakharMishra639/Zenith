3f260f74fee55cad87366f8ab4e222b23bb00183
PrakharMishra639 <mprakhar713@gmail.com>
2025-07-11 01:45:15 +0530

    axiosInstance updated

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const API_BASE_URL = import.meta.env.VITE_API_URL;
+
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: API_BASE_URL,
 });
 
 axiosInstance.interceptors.request.use(
